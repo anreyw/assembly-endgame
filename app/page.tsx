61b5e4d9f259c30c55d6635cab0b317c2504f921
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-07 15:22:24 -0800

    Added max-width to header.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function AssemblyEndgame() {
   return (
-    <header className="text-center">
+    <header className="text-center max-w-md">
       <h1 className="text-amber-100 text-xl font-medium">Assembly: Endgame</h1>
       <p className="text-zinc-300">
 	   Guess the word in under 8 attempts to keep the
