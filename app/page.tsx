1d29c944b04a04330f472e5a9b30e29e8b61c3b2
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-07 15:33:54 -0800

    Moved Header to new component file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
+import Header from '@/app/header'
+
 export default function AssemblyEndgame() {
   return (
-    <header className="text-center max-w-md">
-      <h1 className="text-amber-100 text-xl font-medium">Assembly: Endgame</h1>
-      <p className="text-zinc-300">
-	   Guess the word in under 8 attempts to keep the
-           programming world safe from Assembly!
-      </p>
-    </header>
+    <Header />
   );
 }
