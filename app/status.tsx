9cbdd1be31714d0e66f999016b729d92c368971a
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-07 19:44:30 -0800

    Removed lower case file names.

--- app/status.tsx
+++
@@ -1,8 +0,0 @@
-export default function Status() {
-  return (
-    <section className="flex flex-col items-center py-2 text-white bg-green-600 rounded">
-      <h2 className="font-bold text-xl">YOU WON</h2>
-      <p>Well done! 🎉</p>
-    </section>
-  )
-}
