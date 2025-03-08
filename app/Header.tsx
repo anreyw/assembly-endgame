2fcd4ed4f3f6084e94bef3874c7f012fc24f662c
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-07 19:43:15 -0800

    Change casing of component file names.

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="text-center max-w-md">
+      <h1 className="text-amber-100 text-xl font-medium">Assembly: Endgame</h1>
+      <p className="text-zinc-300">
+	   Guess the word in under 8 attempts to keep the
+           programming world safe from Assembly!
+      </p>
+    </header>
+  );
+}
