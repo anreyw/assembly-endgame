0c06d9fb8805f4ff2dc0dac600bff6511027fd1c
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-08 20:13:25 -0800

    Added New Game Button to root page.

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default function AssemblyEndgame() {
       <LanguageChips />
       <WordDisplay />
       <Keyboard />
+      <button
+	className="py-2 px-5 bg-sky-400 cursor-pointer border border-sky-300 rounded"
+      >New Game</button>
     </main>
   );
 }
