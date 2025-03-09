87e38c432aad60e2bff651d48340b89f8bf81ebd
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-08 21:21:49 -0800

    Added new GuessedLetters state and relevant button click handler.
    - Added new GuessedLetter state to page.tsx
    - Added addGuessedLetter function to page.tsx
    - pass addGuessedLetter function as prop to Keyboard component
    - wired addGuessedLetter to onClick event Listener on Keyboard button

--- app/Keyboard.tsx
+++ app/Keyboard.tsx
@@ -1,10 +1,15 @@
-export default function Keyboard() {
+interface KeyboardProps {
+  handleClick: (letter: string) => void
+}
+
+export default function Keyboard({ handleClick }: KeyboardProps) {
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
   const keyboardEl = alphabet.map(letter => (
     <button
       key={letter}
       className="size-10 bg-yellow-300 rounded border border-amber-100 cursor-pointer"
+      onClick={() => handleClick(letter)}
     >
       {letter.toUpperCase()}
     </button>
--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,20 @@ import Keyboard from '@/app/Keyboard'
 
 export default function AssemblyEndgame() {
   const [ currentWord, setCurrentWord ] = useState('nextjs')
+  const [ guessedLetters, setGuessedLetters ] = useState<string[]>([])
+
+  function addGuessedLetter(letter: string) {
+    setGuessedLetters((prev: string[]) =>
+      prev.includes(letter) ?  prev : [...prev, letter]
+    )
+  }
   
   return (
     <main className="flex flex-col items-center gap-4">
       <Status />
       <LanguageChips />
       <WordDisplay currentWord={currentWord} />
-      <Keyboard />
+      <Keyboard handleClick={addGuessedLetter} />
       <button
 	className="py-2 px-5 bg-sky-400 cursor-pointer border border-sky-300 rounded"
       >New Game</button>
