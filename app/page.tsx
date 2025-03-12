44fd1d2ea4b1934d8676af380c0e155e28baad9f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-03-11 18:31:57 -0700

    Made the New Game button functional.

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,11 @@ export default function AssemblyEndgame() {
       prev.includes(letter) ?  prev : [...prev, letter]
     )
   }
+
+  function startNewGame() {
+    setCurrentWord(getRandomWord)
+    setGuessedLetters([])
+  }
   
   return (
     <main className="flex flex-col items-center gap-4">
@@ -50,7 +55,8 @@ export default function AssemblyEndgame() {
       />
       {isGameOver &&
 	<button
-	className="py-2 px-5 bg-sky-400 cursor-pointer border border-sky-300 rounded"
+	  className="py-2 px-5 bg-sky-400 cursor-pointer border border-sky-300 rounded"
+	  onClick={startNewGame}
 	>
 	   New Game
 	</button>
